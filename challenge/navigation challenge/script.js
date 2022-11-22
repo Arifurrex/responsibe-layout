8a3a89f8719106799404f803a4eb8771e078f0e1
arifur rahman <arifurrex@gmail.com>
2022-11-22 19:30:25 +0600

    responsibe layouts

---
+++ challenge/navigation challenge/script.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+burger.addEventListener('click', function () {
+  nav.classList.toggle('nav--active');
+});
